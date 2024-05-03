f6276d83e658e28ab37f03c29fc2b4bc2f70ec28
amansharma-tech <73787644+amansharma-tech@users.noreply.github.com>
2024-05-03 15:57:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -7,15 +7,15 @@ from tensorflow.keras.models import load_model
 
 # Load preprocessed data
 train_df = pd.read_csv('https://raw.githubusercontent.com/amansharma-tech/PECF/main/train_df.csv')
-df1 = pd.read_csv('https://raw.githubusercontent.com/amansharma-tech/PECF/main/df1.csv')
-X_test = np.load('https://github.com/amansharma-tech/PECF/raw/main/X_test.npy')
-y_test = np.load('https://github.com/amansharma-tech/PECF/raw/main/y_test.npy')
+df1 = pd.read_csv('https://raw.githubusercontent.com/amansharma-tech/PECF/blob/main/df1.csv')
+X_test = np.load('https://github.com/amansharma-tech/PECF/raw/blob/main/X_test.npy')
+y_test = np.load('https://github.com/amansharma-tech/PECF/raw/blob/main/y_test.npy')
 
 # Load trained linear model
-linear_model = load_model('https://github.com/amansharma-tech/PECF/raw/main/linear_model.h5')
+linear_model = load_model('https://github.com/amansharma-tech/PECF/raw/blob/main/linear_model.h5')
 
 # Load test results
-results = pd.read_csv('https://raw.githubusercontent.com/amansharma-tech/PECF/main/test_results.csv')
+results = pd.read_csv('https://raw.githubusercontent.com/amansharma-tech/PECF/blob/main/test_results.csv')
 
 # Calculate evaluation metrics
 mse = mean_squared_error(results['y_test_actual'].tail(48), results['linear_model_pred'].tail(48))
